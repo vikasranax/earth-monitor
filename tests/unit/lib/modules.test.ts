352bf0dc4e7828d808e62956ed5967ac45a7b0a3
vikasrana <viikasranax@gmail.com>
2026-08-19 18:12:13 +0530

    Refined Modules

--- tests/unit/lib/modules.test.ts
+++ tests/unit/lib/modules.test.ts
@@ -7,9 +7,30 @@ describe("modules manifest", () => {
     expect(new Set(ids).size).toBe(ids.length);
   });
 
-  it("has at least M01 through M16 marked online", () => {
+  it("has at least M01 through M40 marked online", () => {
     const onlineIds = new Set(modules.filter((m) => m.status === "online").map((m) => m.id));
-    for (const id of ["M01", "M02", "M03", "M04", "M05", "M06", "M07", "M08", "M09", "M10", "M11", "M12", "M13", "M14", "M15", "M16"]) {
+    for (const id of [
+      "M01",
+      "M02",
+      "M03",
+      "M04",
+      "M05",
+      "M06",
+      "M07",
+      "M08",
+      "M09",
+      "M10",
+      "M11",
+      "M12",
+      "M13",
+      "M14",
+      "M15",
+      "M16",
+      "M27",
+      "M28",
+      "M29",
+      "M34",
+    ]) {
       expect(onlineIds.has(id)).toBe(true);
     }
   });
